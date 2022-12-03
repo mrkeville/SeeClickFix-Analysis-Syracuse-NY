0ca632eb2dc4d1d5d7254961e9af412b66c31c53
mrkeville <98175413+mrkeville@users.noreply.github.com>
2022-12-03 16:54:06 -0500

    Updating code

--- seeclickfix_analysis.R
+++ seeclickfix_analysis.R
@@ -5,7 +5,6 @@ library(sf)
 library(tidyverse)
 library(lubridate)
 library(ggalluvial)
-setwd("H:/IST 719")
 seeclickfix<-read_csv("SeeClickFix_Requests_(2021-Present)__SYRCityline.csv")
 syr_quadrants<-read_sf("Syracuse_Quadrants/Syracuse_Quadrants.shp")
 
@@ -16,7 +15,7 @@ seeclickfix_clean <- seeclickfix %>%
   group_by(Category, Agency_Name)%>%
   mutate(hours_to_closed=Minutes_to_closed/60,hours_to_closed=round(hours_to_closed,2))%>%
   mutate(days_to_closed=Minutes_to_closed/1440,days_to_closed=round(days_to_closed,2))%>%
-  mutate(mean_days_to_closed=mean(days_to_closed, na.rm=T))%>%
+  mutate(mean_days_to_closed=round(mean(days_to_closed, na.rm=T)),0)%>%
   mutate(Created_at_local=mdy_hm(Created_at_local))%>%
   mutate(Acknowledged_at_local=mdy_hm(Acknowledged_at_local))%>%
   mutate(Closed_at_local=mdy_hm(Closed_at_local))%>%
@@ -84,7 +83,8 @@ seeclickfix_clean <- seeclickfix %>%
 
 seeclickfix_clean_cat<-seeclickfix_clean%>%
   group_by(Category)%>%
-  summarize(`Number of Requests`=n())
+  summarize(`Number of Requests`=n())%>%
+  arrange(desc(`Number of Requests`))
 
 ggplot(data=seeclickfix_clean_cat)+
   geom_col(aes(x=reorder(Category, -`Number of Requests`),
@@ -107,10 +107,11 @@ ggsave("requestsbycategory.pdf", height = 10, width=15)
 
 seeclickfix_clean_avgclosed<-seeclickfix_clean%>%
   summarise(days_to_closed=mean(days_to_closed, na.rm=T),
-            `Number of Requests`=n())
+            `Number of Requests`=n())%>%
+  arrange(desc(`Number of Requests`))
 
 ggplot(data=seeclickfix_clean_avgclosed)+
-  geom_col(aes(x=reorder(Category, -days_to_closed),y=days_to_closed, fill= Category))+
+  geom_col(aes(x=reorder(Category, -`Number of Requests`),y=days_to_closed, fill= Category))+
   ggtitle("Average Days to Close Service Requests By Category")+
   xlab("Category")+
   ylab("Mean Number of Days to Close a Request")+
@@ -126,14 +127,16 @@ ggsave("avgdaystoclose_cat.pdf", height=10, width=15)
 
 ##Number of Open Requests----
 
-seeclickfix_clean_openvclose<-seeclickfix_clean%>%
+seeclickfix_clean_open_close<-seeclickfix_clean%>%
   subset(select=-c(Agency_Name))%>%
   summarise(requests_open=sum(is.na(Closed_at_local)),
-            requests_closed=sum(!is.na(Closed_at_local)))
+            requests_closed=sum(!is.na(Closed_at_local)),
+            `Number of Requests`=n())%>%
+  arrange(desc(`Number of Requests`))
 
 
-ggplot(data=seeclickfix_clean_openvclose)+
-  geom_col(aes(x=reorder(Category, -requests_open), y= requests_open, fill=Category))+
+ggplot(data=seeclickfix_clean_open_close)+
+  geom_col(aes(x=reorder(Category, -`Number of Requests`), y= requests_open, fill=Category))+
   ggtitle("Number of Requests Open per Category (as of Aug 2022)")+
   xlab("Category")+
   ylab("Number of Open Requests")+
@@ -149,10 +152,12 @@ ggsave("open_cat.pdf", height=10, width=15)
 
 ##Mean Number of Days to Close A Request per City Agency, by Quadrant----
 
-seeclickfix_clean_quad<-seeclickfix_clean%>%
+seeclickfix_clean_quad_avg<-seeclickfix_clean%>%
   filter(Quadrant!="Syracuse Boundary" & !is.na(Quadrant))%>%
-  group_by(Agency_Name, Quadrant)%>%
-  summarise(days_to_closed=mean(days_to_closed, na.rm=T))
+  group_by(Agency_Name, Quadrant, mean_days_to_closed)%>%
+  summarise(`Number of Requests`=n())%>%
+  arrange(Quadrant,desc(`Number of Requests`))
+
 
 ggplot(data=seeclickfix_clean_quad)+
   geom_col(aes(x=Agency_Name,
@@ -172,13 +177,41 @@ ggplot(data=seeclickfix_clean_quad)+
 
 ggsave("avgclose_quad.pdf", height=10, width=15)
 
+## Number of Requests by Quadrant----
+
+seeclickfix_clean_quad<-seeclickfix_clean%>%
+  filter(Quadrant!="Syracuse Boundary" & !is.na(Quadrant))%>%
+  group_by(Quadrant)%>%
+  summarise(`Number of Requests`=n(),
+            requests_open=sum(is.na(Closed_at_local)),
+            requests_closed=sum(!is.na(Closed_at_local)))%>%
+  arrange(desc(`Number of Requests`))
+
+ggplot(data=seeclickfix_clean_quad)+
+  geom_col(aes(x=reorder(Category, -`Number of Requests`),
+               y=`Number of Requests`,
+               fill=Quadrant))+
+  ggtitle("Requests By Quadrant")+
+  xlab("Quadrant")+
+  ylab("Number of Service Requests")+
+  theme(axis.text.x=element_text(angle=45,hjust=1),
+        legend.position= "none",
+        plot.title = element_text(hjust=0.5),
+        panel.background = element_blank(),
+        panel.grid.major = element_blank())+
+  labs(caption="Data Source: SeeClickFix Requests (2021-Present) SYRCityLine")+
+  scale_fill_viridis_d(option="mako")
+
+ggsave("requestsbyquadrant.pdf", height = 10, width=15)
+
 ##Time plot of requests----
 
 seeclickfix_clean_time<-seeclickfix_clean%>%
   group_by(month_created)%>%
   summarise(`Number of Requests`=n(),
             requests_open=sum(is.na(Closed_at_local)),
-            requests_closed=sum(!is.na(Closed_at_local)))
+            requests_closed=sum(!is.na(Closed_at_local)))%>%
+  arrange(month_created)
 
 ggplot(data=seeclickfix_clean_time, aes(x=month_created))+
   geom_line(aes(y=`Number of Requests`),color="#51C0A8", size=1.5)+
@@ -202,18 +235,19 @@ ggsave("time_requests.pdf", height=10, width=15)
 
 
 
-#Outstanding Requests by Quadrant and Agency----
+## Outstanding Requests by Quadrant and Agency----
 
-seeclickfix_clean <- seeclickfix_clean %>% 
+seeclickfix_clean_outstanding <- seeclickfix_clean %>% 
   mutate(Quadrant=toupper(Quadrant))%>%
   left_join(syr_quadrants, by=c("Quadrant"="NAME"))
 
-seeclickfix_clean_top<-seeclickfix_clean%>%
+seeclickfix_clean_outstanding<-seeclickfix_clean_outstanding%>%
   group_by(Quadrant, Category,Agency_Name)%>%
+  filter(!is.na(Quadrant))%>%
   summarize(`Number of Requests`=n(),
             requests_open=sum(is.na(Closed_at_local)))
 
-ggplot(data=seeclickfix_clean_top,
+ggplot(data=seeclickfix_clean_outstanding,
        aes(y = `requests_open`, 
            axis1 = Quadrant, 
            axis2 = Agency_Name)) + 
@@ -239,4 +273,4 @@ ggplot(data=seeclickfix_clean_top,
   labs(caption="Data Sources: SeeClickFix Requests (2021-Present) SYRCityLine & Syracuse Quandrants")+
   scale_fill_viridis_d(option="mako")
 
-ggsave("outstanding_quad.png", height=10, width= 15)
+ggsave("outstanding_quad.pdf", height=10, width= 15)
